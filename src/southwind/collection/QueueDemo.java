b6f397d3aaa95c9ec183a3f8d651402db9052886
UyCode <Hyper-Hack@outlook.com>
2021-05-07 00:11:10 +0800

    add QueueDemo

--- src/southwind/collection/QueueDemo.java
+++ src/southwind/collection/QueueDemo.java
@@ -11,9 +11,31 @@ import java.util.PriorityQueue;
 public class QueueDemo {
     public static void main(String[] args) {
         PriorityQueue priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(1);
+        /*priorityQueue.add(1);
         priorityQueue.add(2);
-        priorityQueue.add(3);
+        priorityQueue.add(3);*/
         System.out.println(priorityQueue);
+        System.out.println("-------------");
+        priorityQueue.add(new A(2));
+        priorityQueue.add(new A(1));
+        System.out.println(priorityQueue);
+    }
+}
+class A implements Comparable{
+    private int num;
+    public A(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        A a = (A)o;
+        if(this.num > a.num) {
+            return 1;
+        } else if(this.num == a.num) {
+            return 0;
+        } else {
+            return -1;
+        }
     }
 }
