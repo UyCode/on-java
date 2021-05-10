f3f630e7934a478b9581f599b3195e2b0475f7cb
UyCode <Hyper-Hack@outlook.com>
2021-05-10 23:50:18 +0800

    add StringDemo

--- src/southwind/string/StringDemo.java
+++ src/southwind/string/StringDemo.java
@@ -1,5 +1,7 @@
 package southwind.string;
 
+import java.util.Locale;
+
 /**
  * @author ahmatjan(UyCode)
  * @email Hyper-Hack@outlook.com
@@ -8,8 +10,34 @@ package southwind.string;
 
 public class StringDemo {
     public static void main(String[] args) {
-        String str = "Hello World";
-        String str1 = new String("Hello World");
-        System.out.println(str.equals(str1));
+        String str1 = "hello world,this is,Java,course";
+        String str2 = "您好,欢迎来到java的世界";
+        System.out.println(str2.compareTo(str1));
+
+        System.out.println(str1.startsWith("hello world"));
+        System.out.println(str1.indexOf("w"));
+        System.out.println(str1.indexOf("h", 2));
+        System.out.println(str1.substring(6, 11));
+        System.out.println(str1.substring(6));
+        System.out.println(str1.substring(6, str1.length()));
+        System.out.println(str1.concat("."));
+        //System.out.println(str1.replaceAll(str1,"this is new String"));
+        String[] split = str1.split(",");
+        for (String item: split
+             ) {
+            System.out.println(item);
+        }
+
+        System.out.println(str1.toLowerCase());
+        System.out.println(str1.toUpperCase());
+        char[] chars = str2.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i] +" ");
+        }
+
+
+
+
+
     }
 }
