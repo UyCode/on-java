948954a175d75d200144412bbd76c552fa59b3ba
UyCode <Hyper-Hack@outlook.com>
2022-01-06 01:34:18 +0800

    feat: leetcode problem 2

---
+++ src/leetcode/AddTwoNumbers.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * @author ahmatjan(UyCode)
+ * @email Hyper-Hack@outlook.com
+ * @since 1/5/2022
+ */
+
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1!=null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum/10;
+            sum %= 10;
+            current.next = new ListNode(sum);
+            current = current.next;
+        }
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = null;
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        l2.next.next.next = null;
+
+        ListNode result = addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+}
---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * @author ahmatjan(UyCode)
+ * @email Hyper-Hack@outlook.com
+ * @since 1/6/2022
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
